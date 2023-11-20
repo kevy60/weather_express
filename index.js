1d3430d9b2dc3a1b8e75da34cb9775382d027d99
kevy60 <kevin.vorsmann@voco.ee>
2023-11-20 13:36:27 +0200

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch= require('node-fetch')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -10,43 +12,45 @@ app.set('views', path.join(__dirname, 'views'))
 
 
 
-
-
-
-app.get('/', function (req, res) {
-	fetch('https://api.openweathermap.org/data/2.5/weather?q=Tallinn&appid=8bda5516ecfbb26eeebdc1bdebd15076')
-	.then((responce) => {
-		return responce.json()
+const getWeatherDataPromise = (url) => {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+		.then(responce => {
+			return responce.json()
+		})
+		.then(data => {
+			let description = data.weather[0].description
+			let city = data.name
+			let temp = Math.round(parseFloat(data.main.temp))
+			let result = {
+				description: description,
+				city : city,
+				temp: temp
+			}
+			resolve(result)
+		})
+		.catch(error => {
+			reject(error)
+		})
 	})
-	.then((data) => {
-		let description = data.weather[0].description
-		let city = data.name
-		let temp = Math.round(parseFloat(data.main.temp))
-	    res.render('index', {
-	    	description: description,
-	    	city: city,
-	    	temp: temp
-	    })
-	})
+}
+
+
+app.all('/', function (req, res) {
+	let city
+	if(req.method == 'GET'){
+		city = 'Tartu'
+	}
+	if(req.method == 'POST'){
+		city = req.body.cityname
+	}
+	let url = 'https://api.openweathermap.org/data/2.5/weather?q=Tartu&appid=8bda5516ecfbb26eeebdc1bdebd15076'
+    getWeatherDataPromise(url)
+    .then(data => {
+    	res.render('index', data)
+    })
 })
 
-
-app.post('/', function(req, res){
-	fetch('https://api.openweathermap.org/data/2.5/weather?q=Tallinn&appid=8bda5516ecfbb26eeebdc1bdebd15076')
-	.then((responce) => {
-		return responce.json()
-	})
-	.then((data) => {
-		let description = data.weather[0].description
-		let city = data.name
-		let temp = Math.round(parseFloat(data.main.temp))
-	    res.render('index', {
-	    	description: description,
-	    	city: city,
-	    	temp: temp
-	    })
-	})
-})
 
 
 
